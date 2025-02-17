0c4d7116795f685cde91f49a080486e26cc1b14f
Joost de Valk <joost@altha.nl>
2025-02-17 14:40:23 +0100

    CS

--- templates/blank.php
+++ templates/blank.php
@@ -2,7 +2,10 @@
 /**
  * Template for displaying a blank comments page.
  *
+ * @package Comment_Free_Zone
+ *
  * This template is used to display a blank comments page when comments are disabled.
  */
+
 ?>
 <!-- <?php esc_html_e( 'Comments have been disabled.', 'comment-free-zone' ); ?> -->
