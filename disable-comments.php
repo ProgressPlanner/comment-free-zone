c4f35cea125d38f0d46378eeb308928bd52e7049
Joost de Valk <joost@altha.nl>
2025-02-17 12:03:35 +0100

    Initial commit

---
+++ disable-comments.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * A plugin to help you fight procrastination and get things done.
+ *
+ * @package Disable_Comments
+ *
+ * Plugin name:       Disable Comments
+ * Plugin URI:        https://prpl.fyi/disable-comments
+ * Description:       A plugin to fully disable comments on your WordPress site.
+ * Requires at least: 6.3
+ * Requires PHP:      7.4
+ * Version:           1.0.0
+ * Author:            Team Progress Planner
+ * Author URI:        https://prpl.fyi/about
+ * License:           GPL-3.0+
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       disable-comments
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Disable_Comments {
+
+    /**
+     * Constructor.
+     */
+	public function __construct() {
+		add_action( 'init', [ $this, 'disable_comments' ] );
+	}
+
+    /**
+     * Disable comments and trackbacks in post types.
+     */
+	public function disable_comments() {
+        add_action( 'admin_menu', function() {
+		    remove_submenu_page( 'options-general.php', 'options-discussion.php' ); // Comments settings
+		    remove_submenu_page( 'options-general.php', 'options-writing.php' ); // Post settings
+		}, 999 );
+		add_filter( 'manage_pages_columns', [ $this, 'remove_comments_column_from_pages' ] );
+		add_filter( 'comments_open', '__return_false', 20, 2 );
+		add_filter( 'pings_open', '__return_false', 20, 2 );
+
+   		// Disable outgoing pings
+		add_action( 'pre_ping', function() {
+		    return [];
+		});
+
+   		// Disable incoming pingbacks
+		add_filter( 'xmlrpc_methods', function( $methods ) {
+		    unset( $methods[ 'pingback.ping' ] );
+		    return $methods;
+		});
+
+        // Disable support for comments and trackbacks in post types.
+		$post_types = get_post_types();
+		foreach ( $post_types as $post_type ) {
+			if ( post_type_supports( $post_type, 'comments' ) ) {
+				remove_post_type_support( $post_type, 'comments' );
+				remove_post_type_support( $post_type, 'trackbacks' );
+			}
+		}
+	}
+
+    /**
+     * Remove the Comments column from the Pages list table.
+     *
+     * @param array $columns The columns of the Pages list table.
+     * @return array The modified columns.
+     */
+    public function remove_comments_column_from_pages( $columns ) {
+	    unset( $columns[ 'comments' ] ); // Removes the Comments column.
+		
+	    return $columns;
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Disable Comments by Progress Planner ===
+Contributors: joostdevalk, aristath, mariekerakt, irisguelen, samalderson
+Tags: planning, maintenance, writing, blogging
+Requires at least: 6.3
+Tested up to: 6.7
+Requires PHP: 7.4
+Stable tag: 1.0.5
+License: GPL3+
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+A plugin to fully disable comments on your WordPress site
+
+This plugin will:
+
+* Remove the comments menu from the admin
+* Remove the discussion settings page
+* Remove the comments column from posts and pages
+* Disable comments on all post types
+* Disable pingbacks and trackbacks
+* Remove comment support from all post types
+
+Once activated, this plugin requires no configuration - it just works. If you want to completely remove comments from your WordPress site, this is the plugin for you.
+
+== Frequently Asked Questions ==
+
+= Where do I file bugs? =
+
+If you've found a bug, please follow the following steps:
+
+1. If it's a security report, please report it through our [PatchStack Vulnerability Disclosure Program](https://patchstack.com/database/vdp/pp-disable-comments).
+2. If it's not a security report, search through the open issues on [our GitHub repository](https://github.com/ProgressPlanner/disable-comments/issues/) to see if there's already an issue for this problem.
+3. If if doesn't exist yet, file a [bug report on GitHub](https://github.com/ProgressPlanner/disable-comments/issues/new/choose).
+
+== Installation ==
+
+1. Search for Progress Planner Disable Comments on the "Add new plugins" screen in your WordPress plugin.
+2. Install the plugin.
+3. Activate the plugin.
+4. You're done.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial release.
